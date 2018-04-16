1e7407e3f492a5e9a5e7aedaccf3f9194bbf1df8
Riley Mathews <rileymathews80@gmail.com>
2018-04-16 14:00:17 -0500

    added tablet styles

---
+++ uPad.js
@@ -0,0 +1,10 @@
+//create styling elements
+
+let div = document.createElement("div")
+
+div.setAttribute("id", "padTray")
+article.appendChild(div)
+
+let button = document.createElement("div")
+button.setAttribute("id", "homeButton")
+div.appendChild(button)
